09fb5b66bf2bcdc2e135094258c4d2295fb3e719
VagrantPi <xup6u06m4@gmail.com>
2017-02-18 14:49:14 +0800

    新增 LanguageService.js

---
+++ api/services/LanguageService.js
@@ -0,0 +1,104 @@
+module.exports = {
+  create: async ({
+    languageName,
+    languageCode,
+    languageLocal,
+    languageImage,
+    languageDirectory,
+    languageSortOrder,
+    languageStatus
+  }) => {
+    try {
+      sails.log.info({
+        languageName,
+        languageCode,
+        languageLocal,
+        languageImage,
+        languageDirectory,
+        languageSortOrder,
+        languageStatus
+      });
+
+      const findExistLanguage = await Language.find({
+        where: { name: languageName }
+      });
+
+      if (findExistLanguage) {
+        throw new Error(`user ${findExistLanguage.languageName} exist!`);
+      }
+
+      const language = await Language.create({
+        languageName,
+        languageCode,
+        languageLocal,
+        languageImage,
+        languageDirectory,
+        languageSortOrder,
+        languageStatus
+      });
+
+      return language;
+    } catch (e) {
+      sails.log.error(e);
+    }
+  },
+
+  findByLanguageName: async(languageName) => {
+    try {
+      return await Language.find({ where: { name:LanguageName } });
+    } catch (e) {
+      throw e;
+    }
+  },
+
+  update: async (language = {
+    name,
+    code,
+    local,
+    image,
+    directory,
+    sortOrder,
+    status
+  }) => {
+    try {
+      sails.log.info('update language service=>', language)
+      let updatedLanguage = await Language.findOne({
+        where: {
+          name: language.name;
+        }
+      });
+
+      if(updatedLanguage) {
+        updatedLanguage.name = language.name;
+        updatedLanguage.code = language.code;
+        updatedLanguage.local = language.local;
+        updatedLanguage.image = language.image;
+        updatedLanguage.directory = language.directory;
+        updatedLanguage.sortOrder = language.sortOrder;
+        updatedLanguage.status = language.status;
+      } else {
+        sails.log.warn(`${language.name} can't find!`);
+      }
+
+      updatedLanguage = await updatedLanguage.save();
+      return updatedLanguage;
+    } catch (e) {
+      throw e;
+    }
+  },
+  
+  deleteByLanguageName: async(languageName) => {
+    try {
+      deletedLangue = await Language.destroy({
+        where: {
+          name: languageName;
+        }
+      });
+      if(deletedLangue) {
+        sails.log.info(`${languageName} is destroy!`);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+}
