8ac59b4444e75413ecdb3093af913dd0b9e2f324
hsiangtzu <a841225@gmail.com>
2017-07-14 14:46:35 +0800

    productDetail 左上雲端魚場”改成後台可設定

--- api/controllers/b2b/ProductController.js
+++ api/controllers/b2b/ProductController.js
@@ -14,25 +14,25 @@ module.exports = {
        */
       let {start, length, category, supplier, limit, q, sort, sortDir = 'asc'} = req.query;
       category = Number(category);
-      
+
       if( !category && q ) {
         category = 0;
       } else if ( !category && !q ) {
         category = 1;
-      } 
+      }
       // 防錯
       if(sort && sort.split('|').length > 1)
         [sort, sortDir] = sort.split('|');
       sort = UtilsService.findInArray(['price', 'time'], sort);
       sortDir = UtilsService.findInArray(['asc', 'desc'], sortDir.toLowerCase());
-      
+
       const result = await ProductService.find({
         start,
         length,
         supplierId: supplier,
         categoryId: category,
         limit,
-        keyword: q, 
+        keyword: q,
         sortBy: (sort === 'time') ? 'createdAt' : sort,
         sortDir
       });
@@ -45,7 +45,7 @@ module.exports = {
       // categorys = categorys.map(function( category ){
       //   return category.CategoryDescription.name;
       // });
-      
+
       q = (!q) ? '' : q;
 
       res.view('index',
@@ -75,14 +75,14 @@ module.exports = {
           id: req.params.id
         },
         include: [ProductDescription, ProductOption, ProductOptionValue, ProductImage],
-        layoutImages: {
-          bannerLogo: sails.config.layoutImages.bannerLogo[0],
-        }
       });
       sails.log('banner=>', sails.config.layoutImages.bannerLogo)
       res.view('b2b/product/detail',{
         data: {
           item,
+        },
+        layoutImages: {
+          bannerLogo: sails.config.layoutImages.bannerLogo[0],
         }
       });
     } catch (e) {
